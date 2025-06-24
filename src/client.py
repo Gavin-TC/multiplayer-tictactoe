997e97c7b71ad5d010fd0c1e58076690b68b4c0a
Gavin-TC <gtclonts@gmail.com>
2025-06-24 18:41:07 -0400

    fixed basically all issues with client + completed network logic; server not detecting both players disconnecting

--- src/client.py
+++ src/client.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from time import sleep
 import socket
 import threading
 import time
@@ -10,57 +9,130 @@ PORT = 5555
 
 class ClientState(Enum):
     SHUTTING_DOWN = 1
-    PLAYING = 2
-    AWAITING_CONNECTION = 3
+    AWAITING_SHUTDOWN = 2
+    PLAYING = 3
+    AWAITING_CONNECTION = 4
+    HANDLING_ERROR = 5
+
+class GameState(Enum):
+    WON = 1
+    LOST = 2
+    DRAW = 3
+    PLAYING = 4
 
 board_data = ""
 board = []
 board_ready_event = threading.Event()
+board_updated = threading.Event()
+shutdown_event = threading.Event()
 player_num = -1
 my_turn = False
 
-def main() -> None:
-    game_running = True
-    shared_state = {"cur_state": ClientState.PLAYING}
+# Basically a flag that allows the next frame to be rendered
+advance_frame = True
+move_accepted = False
+game_running = True
+game_over = False
 
-    client_socket = attempt_server_connect()
+
+def main() -> None:
+    global my_turn, advance_frame, move_accepted, game_running, game_over
+
+    shared_state = {"cur_state": ClientState.PLAYING, "game_state": GameState.PLAYING}
+
+    client_socket: socket.socket = attempt_server_connect(shared_state)
 
     print(f"Connected to server at {HOST}:{PORT}")
+    print(f"Waiting for game to start...")
 
     threading.Thread(target=listen, args=(client_socket, shared_state), daemon=True).start()
-    threading.Thread(target=handle_state, args=(client_socket, shared_state), daemon=True).start()
 
     board_ready_event.wait()
 
-    while game_running and shared_state["cur_state"] == ClientState.PLAYING:
-        for row in board:
-            for char in row:
-                print(char, end="")
-            print()
+    while game_running:
+        if advance_frame:
+            advance_frame = False
 
-        if my_turn == True:
-            move = input("Enter a move: ")
-            match move:
-                case 'q':
-                    shared_state["cur_state"] = ClientState.SHUTTING_DOWN
-            client_socket.send(("move:" + str(move)).encode())
-        else:
-            print("Wait for your turn...")
+            clear_terminal()
+            print_board()
 
+            if game_over:
+                game_running = False
+                my_turn = False
+                shared_state["cur_state"] = ClientState.AWAITING_SHUTDOWN
+
+            if my_turn:
+                while not move_accepted:
+                    move = input("Your move (1-9, or 'q' to quit): ")
+
+                    match move:
+                        case "q":
+                            shared_state["cur_state"] = ClientState.SHUTTING_DOWN
+                            break
+
+                    if move.isdigit():
+                        client_socket.send(("move:" + str(move)).encode())
+                    time.sleep(0.25) # Allow time for server to process move
+                move_accepted = False
+            elif not game_over:
+                print("Waiting for your turn...")
+
+        # State should always be checked, not just during frame render
         match shared_state["cur_state"]:
+            case ClientState.HANDLING_ERROR:
+                print("Caught error...")
+                game_running = False
+
+            case ClientState.SHUTTING_DOWN:
+                print("Quitting...")
+                game_running = False
+
+            case ClientState.AWAITING_SHUTDOWN:
+                pass
+
             case ClientState.AWAITING_CONNECTION:
-                attempt_server_connect()
+                print("Attempting server reconnect...")
+                attempt_server_connect(shared_state)
+                time.sleep(1)
 
-    client_socket.send("closing".encode())
+    match shared_state["game_state"]:
+        case GameState.WON:
+            print("You won!")
+
+        case GameState.LOST:
+            print("You lost!")
+
+        case GameState.DRAW:
+            print("Draw!")
+    print("Game Over!")
+
+    shutdown_event.set()
+    time.sleep(0.5)
+
+    try:
+        client_socket.send("info:closing".encode())
+    except Exception as e:
+        print(f"[ERROR] Ran into error when sending closing message: {e}")
+
+    try:
+        client_socket.shutdown(socket.SHUT_RDWR)
+        client_socket.close()
+    except Exception as e:
+        print(f"[ERROR] Ran into error when closing connection: {e}")
+
+    client_socket.close()
 
 
-def attempt_server_connect() -> socket.socket | None:
+def attempt_server_connect(cur_state) -> socket.socket:
     while True:
         try:
             client_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client_socket.connect((HOST, PORT))
 
             clear_terminal()
+
+            # The player is now connected, they can begin playing.
+            cur_state["cur_state"] = ClientState.PLAYING
 
             return client_socket
         except:
@@ -74,14 +146,12 @@ def clear_terminal() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def listen(sock: socket.socket, cur_state):
-    global board, my_turn
+def listen(sock: socket.socket, cur_state) -> None:
+    global board, player_num, my_turn, advance_frame, move_accepted, game_running, game_over, shutdown_event
 
-    while True:
+    while game_running and not shutdown_event.is_set():
         try:
-            msg = sock.recv(1024).decode()
-            print(f"msg: '{msg}'")
-            type, data = msg.split(":")
+            type, data = sock.recv(1024).decode().split(":", 1)
 
             match type:
                 case "pnum":
@@ -89,23 +159,58 @@ def listen(sock: socket.socket, cur_state):
                     player_num = data
 
                 case "board":
-                    board = data
-                    print(f"Received board: {board}")
-                    parse_board(board)
+                    parse_board(data)
+                    board_updated.set()
+                    advance_frame = True
 
+                # Check what the data is, because this tag is multi-purpose
                 case "info":
-                    match data:
+                    match data.split(":", 1)[0]:
                         case "closing":
                             cur_state["cur_state"] = ClientState.AWAITING_CONNECTION
+
                         case "yourturn":
+                            board_data = data.split(":", 1)[1]
+                            print(f"Board data: {board_data}")
+                            parse_board(board_data)
+                            advance_frame = True
                             my_turn = True
+
+                        case "move_accepted":
+                            advance_frame = True
+                            move_accepted = True
+                            my_turn = False
+
+                        case "move_declined":
+                            print("Invalid move! Try again.")
+
+                        case "win":
+                            print("You won!")
+                            board_data = data.split(":", 1)[1]
+                            parse_board(board_data)
+                            game_over = True
+                            cur_state["game_state"] = GameState.WON
+
+                        case "lost":
+                            print("You lost!")
+                            board_data = data.split(":", 1)[1]
+                            parse_board(board_data)
+                            game_over = True
+                            cur_state["game_state"] = GameState.LOST
+
+                        case "draw":
+                            print("The game is a draw!")
+                            board_data = data.split(":", 1)[1]
+                            parse_board(board_data)
+                            game_over = True
+                            cur_state["game_state"] = GameState.DRAW
         except Exception as e:
             print(f"[ERROR] Ran into error while listening: {e}")
-            break
+            cur_state["cur_state"] = ClientState.HANDLING_ERROR
 
 
 def parse_board(board_str: str) -> None:
-    global board
+    global board, advance_frame
 
     board_str = board_str[:-1]
     rows = board_str.split(",")
@@ -113,14 +218,14 @@ def parse_board(board_str: str) -> None:
     board = [list(row) for row in rows]
     board_ready_event.set()
 
+    advance_frame = True
 
-# kinda useless atm
-def handle_state(sock: socket.socket, cur_state) -> None:
-    while True:
-        match cur_state["cur_state"]:
-            case ClientState.SHUTTING_DOWN:
-                print("Shutting client down...")
-                break
+
+def print_board() -> None:
+    for row in board:
+        for char in row:
+            print(char, end="")
+        print()
 
 
 main()
--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from nt import truncate
 import socket
 import threading
 import time
@@ -20,48 +21,125 @@ addrs = []
 
 cur_turn = 1
 cur_move = -1
+advance_turn = True
+move_accepted = False
 
 game_running = True
 board = [["." for _ in range(3)] for _ in range(3)]
 
 
 def main() -> None:
+    global game_running, cur_turn, cur_move, advance_turn, move_accepted
+
     await_connections()
 
-    # Send each player a copy of the board.
+    # Send each player a copy of the board and their assigned player number.
     for player in players:
-        print(f"Sending pnum + board to player #{players.index(player)}")
+        print(f"Sending `pnum` and `board` to Player #{players.index(player) + 1}")
         player.send(("pnum:" + str(players.index(player) + 1)).encode())
+        time.sleep(0.2)
         player.send(("board:" + get_board_as_str()).encode())
 
     threading.Thread(target=handle_player, args=(player1, 1), daemon=True).start()
     threading.Thread(target=handle_player, args=(player2, 2), daemon=True).start()
 
-    input()
-
+    print("[SERVER] Game is starting!")
     while game_running:
-        players[cur_turn - 1].send("info:yourturn".encode())
+        if is_draw(board):
+            print("[SERVER] Game is a draw!")
+            game_running = False
+            break
 
-    print("Shutting down server...")
-    for player in players:
-        player.send("info:closing".encode())
+        players[cur_turn - 1].send(("info:yourturn:" + get_board_as_str()).encode())
+
+        while move_accepted == False:
+            pass
+        move_accepted = False
+    try:
+        for player in players:
+            player.send("info:closing".encode())
+    except Exception as e:
+        print(f"[SERVER][ERROR] Error occurred when shutting down: {e}")
+
+    print("Server successfully shut down.")
     server_socket.close()
 
 
+def is_valid(move) -> bool:
+    if move.isdigit():
+        move = int(move) - 1
+        if 0 <= move <= 8 and board[move // 3][move % 3] == ".":
+            return True
+    return False
+
+def is_win(board) -> bool:
+    # Check rows
+    for row in board:
+        if row[0] == row[1] == row[2] != ".":
+            return True
+
+    # Check columns
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != ".":
+            return True
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != ".":
+        return True
+    if board[0][2] == board[1][1] == board[2][0] != ".":
+        return True
+
+    return False
+
+
+def is_draw(board):
+    return all(cell != "." for row in board for cell in row)
+
+
 def handle_player(sock: socket.socket, player_num: int) -> None:
+    global cur_turn, board, move_accepted
+
     while True:
         print(f"Awaiting message from Player {player_num}...")
         try:
             msg = sock.recv(1024).decode()
-            type, data = msg.split(":")
+            print(f"Received message from Player {player_num}: `{msg}`")
+            type, data = msg.split(":", 1)
 
-            if cur_turn == player_num:
-                pass
+            match type:
+                case "move":
+                    if cur_turn == player_num:
+                        move = data
+                        if is_valid(move):
+                            print(f"[SERVER: INFO] Move `{move}` is valid.")
+                            sock.send("info:move_accepted".encode())
+                            board[(int(move) - 1) // 3][(int(move) - 1) % 3] = "X" if player_num == 1 else "O"
 
+                            # After every valid move, check if the game is over.
+                            if is_win(board):
+                                print(f"[SERVER: INFO] Player {player_num} wins!")
+                                sock.send(("info:win:" + get_board_as_str()).encode())
+                                print(f"[SERVER: INFO] Player {player_num % 2 + 1} lost!")
+                                players[player_num % 2].send(("info:lost:" + get_board_as_str()).encode())
+                                return
+                            else:
+                                cur_turn = cur_turn % 2 + 1
+                                move_accepted = True
+                                time.sleep(0.1) # Give time for client to receive previous message
+                                for player in players:
+                                    player.send(("board:" + get_board_as_str()).encode())
+                        else:
+                            print(f"[SERVER] Move `{move}` is invalid.")
+                            sock.send("info:move_declined".encode())
+                            cur_turn = cur_turn % 2 + 1
+                case "info":
+                    match data:
+                        case "closing":
+                            print(f"[SERVER: INFO] Player {player_num} disconnected.")
         except Exception as e:
             if e.__class__ is ConnectionResetError:
                 await_reconnect()
-            print(f"[ERROR]: {e}")
+            print(f"[ERROR] While handling Player {player_num}: {e}")
             break
 
 
@@ -78,6 +156,7 @@ def await_connections() -> None:
             players.append(player1)
             addrs.append(player1_addr)
             print("Player 1 has connected...")
+
         elif not player2:
             player2 = client_socket
             player2_addr = client_addr
@@ -94,24 +173,16 @@ def await_connections() -> None:
 # [UNFINISHED]
 def await_reconnect() -> None:
     return
-    secs = 0
-    thresh = 20
-
-    while secs < thresh:
-        time.sleep(1)
 
 
 def get_board_as_str() -> str:
     global board
-
     result = ""
 
     for row in board:
         for piece in row:
             result += str(piece)
         result += "," # Delimiter
-
-    print(result)
     return result
 
 
